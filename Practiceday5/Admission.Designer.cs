bec86a52cd859f96b04f96b8b963d462ced99dcc
munnafdewan <engr.munnaf@gmail.com>
2019-09-20 20:39:15 +0600

    studentinfoadded

---
+++ Practiceday5/Admission.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace Practiceday5
+{
+    partial class Admission
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gpaTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.idRadio = new System.Windows.Forms.RadioButton();
+            this.nameRadio = new System.Windows.Forms.RadioButton();
+            this.mobileRadio = new System.Windows.Forms.RadioButton();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.maxTextBox = new System.Windows.Forms.TextBox();
+            this.mimTextBox = new System.Windows.Forms.TextBox();
+            this.nameMaxTextBox = new System.Windows.Forms.TextBox();
+            this.mimNameTextBox = new System.Windows.Forms.TextBox();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.averegeTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.gpaTextBox);
+            this.groupBox1.Controls.Add(this.addressTextBox);
+            this.groupBox1.Controls.Add(this.ageTextBox);
+            this.groupBox1.Controls.Add(this.phoneTextBox);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.idTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(390, 283);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(236, 85);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 13;
+            this.showButton.Text = "Show All";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(236, 46);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 12;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "GPA Point";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mobile";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name";
+            // 
+            // gpaTextBox
+            // 
+            this.gpaTextBox.Location = new System.Drawing.Point(79, 224);
+            this.gpaTextBox.Name = "gpaTextBox";
+            this.gpaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.gpaTextBox.TabIndex = 6;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(79, 184);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(79, 146);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 4;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(79, 105);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(100, 20);
+            this.phoneTextBox.TabIndex = 3;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(79, 65);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(79, 28);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(459, 12);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(329, 321);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // idRadio
+            // 
+            this.idRadio.AutoSize = true;
+            this.idRadio.Location = new System.Drawing.Point(32, 310);
+            this.idRadio.Name = "idRadio";
+            this.idRadio.Size = new System.Drawing.Size(36, 17);
+            this.idRadio.TabIndex = 2;
+            this.idRadio.TabStop = true;
+            this.idRadio.Text = "ID";
+            this.idRadio.UseVisualStyleBackColor = true;
+            // 
+            // nameRadio
+            // 
+            this.nameRadio.AutoSize = true;
+            this.nameRadio.Location = new System.Drawing.Point(32, 333);
+            this.nameRadio.Name = "nameRadio";
+            this.nameRadio.Size = new System.Drawing.Size(53, 17);
+            this.nameRadio.TabIndex = 3;
+            this.nameRadio.TabStop = true;
+            this.nameRadio.Text = "Name";
+            this.nameRadio.UseVisualStyleBackColor = true;
+            // 
+            // mobileRadio
+            // 
+            this.mobileRadio.AutoSize = true;
+            this.mobileRadio.Location = new System.Drawing.Point(32, 356);
+            this.mobileRadio.Name = "mobileRadio";
+            this.mobileRadio.Size = new System.Drawing.Size(56, 17);
+            this.mobileRadio.TabIndex = 4;
+            this.mobileRadio.TabStop = true;
+            this.mobileRadio.Text = "Mobile";
+            this.mobileRadio.UseVisualStyleBackColor = true;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(303, 310);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(520, 398);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(504, 372);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "Avarage";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(358, 399);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 39;
+            this.label9.Text = "Name";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(367, 372);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 13);
+            this.label10.TabIndex = 38;
+            this.label10.Text = "Mim";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(201, 396);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 13);
+            this.label11.TabIndex = 37;
+            this.label11.Text = "Name";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(188, 371);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 36;
+            this.label12.Text = "Max gpa";
+            // 
+            // maxTextBox
+            // 
+            this.maxTextBox.Location = new System.Drawing.Point(245, 369);
+            this.maxTextBox.Name = "maxTextBox";
+            this.maxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.maxTextBox.TabIndex = 35;
+            // 
+            // mimTextBox
+            // 
+            this.mimTextBox.Location = new System.Drawing.Point(396, 369);
+            this.mimTextBox.Name = "mimTextBox";
+            this.mimTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mimTextBox.TabIndex = 34;
+            // 
+            // nameMaxTextBox
+            // 
+            this.nameMaxTextBox.Location = new System.Drawing.Point(245, 394);
+            this.nameMaxTextBox.Name = "nameMaxTextBox";
+            this.nameMaxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameMaxTextBox.TabIndex = 33;
+            // 
+            // mimNameTextBox
+            // 
+            this.mimNameTextBox.Location = new System.Drawing.Point(396, 400);
+            this.mimNameTextBox.Name = "mimNameTextBox";
+            this.mimNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mimNameTextBox.TabIndex = 32;
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(560, 396);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalTextBox.TabIndex = 31;
+            // 
+            // averegeTextBox
+            // 
+            this.averegeTextBox.Location = new System.Drawing.Point(560, 370);
+            this.averegeTextBox.Name = "averegeTextBox";
+            this.averegeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.averegeTextBox.TabIndex = 30;
+            // 
+            // Admission
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.maxTextBox);
+            this.Controls.Add(this.mimTextBox);
+            this.Controls.Add(this.nameMaxTextBox);
+            this.Controls.Add(this.mimNameTextBox);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.averegeTextBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.mobileRadio);
+            this.Controls.Add(this.nameRadio);
+            this.Controls.Add(this.idRadio);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Admission";
+            this.Text = "Admission";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox gpaTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.RadioButton idRadio;
+        private System.Windows.Forms.RadioButton nameRadio;
+        private System.Windows.Forms.RadioButton mobileRadio;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox maxTextBox;
+        private System.Windows.Forms.TextBox mimTextBox;
+        private System.Windows.Forms.TextBox nameMaxTextBox;
+        private System.Windows.Forms.TextBox mimNameTextBox;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.TextBox averegeTextBox;
+    }
+}
+
---
+++ Practiceday5/Admission.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practiceday5
+{
+    public partial class Admission : Form
+    {
+
+        ErrorProvider erp = new ErrorProvider();
+
+        
+        List<string> ids = new List<string> ();
+        List<string> names = new List<string> ();
+        List<string> mobiles = new List<string> ();
+        List<string> ages = new List<string> ();
+        List<string> addres = new List<string> ();
+        List<string> gpas = new List<string> ();
+
+        string message = " ";
+        int index = 0;
+        public Admission()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            erp.Clear();
+
+            if (idTextBox.Text.Equals(" ") || idTextBox.Text.Length != 4)
+            {
+                index++;
+                erp.SetError(idTextBox, "Enter your Id in 4 character");
+                return;
+            }
+            else if (checkDuplicateID()) ;
+            else if (nameTextBox.Text.Equals(" ") || nameTextBox.Text.Length > 30)
+            {
+                index++;
+                erp.SetError(nameTextBox, "Enter your Name less then 30 character");
+                return;
+            }
+
+            else if (phoneTextBox.Text.Equals(" ") || phoneTextBox.Text.Length != 11)
+            {
+                index++;
+                erp.SetError(phoneTextBox, "Enter your phone number in 11 character");
+                return;
+            }
+            else if (checkDuplicateMobile()) ;
+
+            else if (ageTextBox.Text.Equals(" ") || ageTextBox.Text.Length >50)
+            {
+                index++;
+                erp.SetError(ageTextBox, "Enter your age ");
+                return;
+            }
+
+            else if (addressTextBox.Text.Equals(" ") )
+            {
+                index++;
+                erp.SetError(addressTextBox, "Write your address ");
+                return;
+            }
+
+            else if (gpaTextBox.Text.Equals(" ") || (float.Parse(gpaTextBox.Text) < 1 || float.Parse(gpaTextBox.Text) > 4))
+            {
+                index++;
+                erp.SetError(gpaTextBox, "GPA point must be in 1-4 ");
+                return;
+            }
+
+            ids.Add(idTextBox.Text);
+            names.Add(nameTextBox.Text);
+            mobiles.Add(phoneTextBox.Text);
+            ages.Add(ageTextBox.Text);
+            addres.Add(addressTextBox.Text);
+            gpas.Add(gpaTextBox.Text);
+
+            richTextBox.Text = "ID : " + idTextBox.Text + "\n" + "Name : " + nameTextBox.Text + "\n" + "Mobile : " + phoneTextBox.Text + "\n" + "Age : " + ageTextBox.Text + "\n" + "Address : " + addressTextBox.Text + "\n" + "GPA : " + gpaTextBox.Text + "\n";
+            MessageBox.Show("Student is admitted");
+            Clear();
+             
+        }
+
+      
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            String minName = " ", maxName = " ";
+            float min = 10, max = 0, avg = 0, total = 0, gpatext;
+
+
+            for(int j = 0; j<ids.Count(); j++)
+            {
+                gpatext = float.Parse(gpas[j]);
+                    total += gpatext;
+
+                if(min > gpatext)
+                {
+                    min = gpatext;
+                    minName = names[j];
+                }
+
+                if(max < gpatext)
+                {
+                    max = gpatext;
+                    maxName = names[j];
+
+                }
+
+                message += "ID : " + ids[j] + "\n" + "Name : " + names[j] + "\n" + "Mobile : " + mobiles[j] + "\n" + "Age : " + ages[j] + "\n" + "Address : " + addres[j] + "\n" + "GPA : " + gpas[j] + "\n" + "\n";
+
+                maxTextBox.Text = max.ToString();
+                nameMaxTextBox.Text = maxName;
+                mimTextBox.Text = min.ToString();
+                mimNameTextBox.Text = minName;
+                totalTextBox.Text = total.ToString();
+                averegeTextBox.Text = (total / ids.Count()).ToString();
+            }
+            richTextBox.Text = message;
+           
+        }
+
+        private void Clear()
+        {
+            nameTextBox.Text = "";
+            idTextBox.Text = "";
+            phoneTextBox.Text = "";
+            ageTextBox.Text = "";
+            addressTextBox.Text = "";
+            gpaTextBox.Text = "";
+            maxTextBox.Text = "";
+            mimTextBox.Text = "";
+            nameMaxTextBox.Text = "";
+            mimNameTextBox.Text = "";
+            averegeTextBox.Text = "";
+            totalTextBox.Text = "";
+
+        }
+
+        private void ShowStudent()
+        {
+           
+            int id = Convert.ToInt32(idTextBox.Text);
+            string name = nameTextBox.Text;
+            string mobile = phoneTextBox.Text;
+            int age = Convert.ToInt32(ageTextBox.Text);
+            string address = addressTextBox.Text;
+            double gpa = Convert.ToDouble(gpaTextBox.Text);
+
+            for (int i = 0; i < names.Count(); i++)
+            {
+                message +="Id " + id + "\n" +"Name : " + name[index] + "\n" + "Mobile :" + mobile[index] + "\n " + "Age :" + age + "\n" + "Address :" + address[index] + "\n" + "GPA :" + gpa;
+
+            }
+
+            richTextBox.Text = (message);
+        }
+
+        private Boolean checkDuplicateID()
+        {
+            for(int i =0;i<ids.Count();i++)
+            {
+                if((idTextBox.Text).Equals(ids[i]))
+                {
+                    index++;
+                    erp.SetError(idTextBox, "Id must be unique");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        private Boolean checkDuplicateMobile()
+        {
+            for (int i = 0; i < mobiles.Count(); i++)
+            {
+                if ((phoneTextBox.Text).Equals(mobiles[i]))
+                {
+                    index++;
+                    erp.SetError(phoneTextBox, "Phone number must be unique");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (idRadio.Checked == true)
+            {
+                richTextBox.Text = "";
+                for (int i = 0; i < ids.Count(); i++)
+                {
+                    if (idTextBox.Text.Equals(ids[i]))
+                    {
+                        richTextBox.Text = "ID: " + ids[i] + "\n" + "Name: " + names[i] + "\n" + "Mobile: " + mobiles[i] + "\n" + "Age: " + ages[i] + "\n" +
+                       "Address: " + addres[i] + "\n" + "GPA: " + gpas[i];
+
+                    }
+                    idTextBox.Text = "";
+                    Clear();
+                }
+            }
+
+            else if (nameRadio.Checked == true)
+            {
+                richTextBox.Text = "";
+                for (int i = 0; i < names.Count(); i++)
+                {
+                    if (nameTextBox.Text.Equals(names[i]))
+                    {
+                        richTextBox.Text = "ID: " + ids[i] + "\n" + "Name: " + names[i] + "\n" + "Mobile: " + mobiles[i] + "\n" + "Age: " + ages[i] + "\n" +
+                      "Address: " + addres[i] + "\n" + "GPA: " + gpas[i];
+                    }
+                    nameTextBox.Text = "";
+                    Clear();
+                }
+            }
+
+            else if (mobileRadio.Checked == true)
+            {
+                richTextBox.Text = "";
+                for (int i = 0; i < mobiles.Count(); i++)
+                {
+                    if (phoneTextBox.Text.Equals(mobiles[i]))
+                    {
+                        richTextBox.Text = "ID: " + ids[i] + "\n" + "Name: " + names[i] + "\n" + "Mobile: " + mobiles[i] + "\n" + "Age: " + ages[i] + "\n" +
+                    "Address: " + addres[i] + "\n" + "GPA: " + gpas[i];
+
+                    }
+                    phoneTextBox.Text = "";
+                    Clear();
+                }
+            }
+        }
+    }
+}
